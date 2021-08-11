21f6abb1e5aaa14580a2cc75a591aab2fc7cb9ef
Bilal-11 <sbilalahmad2000@gmail.com>
2021-08-11 10:05:21 +0530

    Added function prototype and definition skeleton

--- custom_pow_function.c
+++ custom_pow_function.c
@@ -1,8 +1,15 @@
 /*Custom power function, made using looping construct*/
 /*Author: Bilal, Date: 11/08/2021*/
 #include<stdio.h>
+int power(int,int);
 
 int main()
+{
+
+  return 0;
+}
+
+int power(int a,int b)
 {
 
   return 0;
